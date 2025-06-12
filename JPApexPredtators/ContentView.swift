119b03238e1a3eeca8b39dca6deb008df6c184fa
Júlio Andherson <julioandherson1@gmail.com>
2025-06-11 23:57:37 -0300

    initial commit

---
+++ JPApexPredtators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredtators
+//
+//  Created by Júlio Andherson de Oliveira Silva on 11/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
