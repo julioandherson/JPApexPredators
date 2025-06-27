f45517dcfb7140cce1943baeaa1dca049576ed15
Júlio Andherson <julioandherson1@gmail.com>
2025-06-26 23:24:40 -0300

    creating details

---
+++ JPApexPredtators/PredatorDetail.swift
@@ -0,0 +1,87 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredtators
+//
+//  Created by Júlio Andherson de Oliveira Silva on 23/06/25.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    let predator: ApexPredator
+    
+    var body: some View {
+        GeometryReader { geo in
+        
+            ScrollView {
+                
+                ZStack(alignment: .bottomTrailing) {
+                    // Background Image
+                    Image(predator.type.rawValue)
+                        .resizable()
+                        .scaledToFit()
+                        
+                    
+                    // Dinossaur Image
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width / 1.5, height: geo.size.height / 3.7)
+                        .scaleEffect(x: -1)
+                        .shadow(color: .black, radius: 7)
+                        .offset(y: 20)
+                }
+                
+                VStack(alignment: .leading) {
+                    // Dino Name
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    
+                    // Current location
+                    
+                    // Appear in
+                    
+                    Text("Appears in:")
+                        .font(.title3)
+                    
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("•" + movie)
+                            .font(.subheadline)
+                    }
+                    
+                    // Movie Moments
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    
+                    ForEach(predator.movieScenes) { scene in
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                    }
+                    
+                    // Links WebPage
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination: URL(string: predator.link)!)
+                        .font(.caption)
+                        .foregroundStyle(.blue)                    
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
+            }
+        }
+        .ignoresSafeArea()
+
+        
+
+    }
+}
+
+#Preview {
+    PredatorDetail(predator: Predators().apexPredators[2])
+}
